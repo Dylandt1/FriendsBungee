9ade14c1fb52d22b0fef0f4c49db826f1746589d
PataPom <dylan@DELL-L7480-01.hbdcloud.local>
2024-07-23 23:45:16 +0200

    Add licence part to ChatListener class

--- src/main/java/fr/patapom/friendsbg/fbg/cmd/CmdReport.java
+++ src/main/java/fr/patapom/friendsbg/fbg/cmd/CmdReport.java
@@ -10,9 +10,25 @@ import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
 import net.md_5.bungee.config.Configuration;
 
-import java.io.ObjectInputFilter;
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * This file is part of FriendsBungee, a BungeeCord friends plugin system.
+ *
+ * FriendsBungee is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FriendsBungee is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 public class CmdReport extends Command implements TabExecutor
 {
--- src/main/java/fr/patapom/friendsbg/fbg/listeners/ChatListener.java
+++ src/main/java/fr/patapom/friendsbg/fbg/listeners/ChatListener.java
@@ -10,6 +10,23 @@ import net.md_5.bungee.event.EventHandler;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This file is part of FriendsBungee, a BungeeCord friends plugin system.
+ *
+ * FriendsBungee is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FriendsBungee is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 public class ChatListener implements Listener
 {
     private final boolean antiSpam = FriendsBG.getInstance().getConfig().getBoolean("msg.antiSpam.use");
